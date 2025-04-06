e02d073298727733dad4082f25ab9c3b66d4d8de
Davi Silva <davisilvaphoto@gmail.com>
2025-04-06 15:41:48 -0300

    create ping/pong route

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"my-contacts/internal/handler"
+	"my-contacts/internal/infra"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	infra.InitDB()
+	defer infra.DB.Close()
+
+	router := gin.Default()
+
+	router.GET("/ping", handler.Ping)
+
+	log.Println("🚀 Server running at http://localhost:8080")
+	router.Run(":8080")
+}
